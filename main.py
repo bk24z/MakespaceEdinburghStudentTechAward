db35d4c3fc31b132effc5108731a460f0850feb2
bk24z <bradleyaustin2611@gmail.com>
2025-11-01 00:56:38 +0000

    Refactor ball and trajectory handling into classes, update movement logic

--- main.py
+++ main.py
@@ -15,54 +15,50 @@ class Ball:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.img = BallImage(self)
+        self.moving = False
+    def move(self):
+        pass
 
 class BallImage:
     def __init__(self, ball):
         self.ball = ball
-        self.img = pygame.transform.rotozoom(ball_img_original, 0, ball_scale)
-        self.img_width = self.img.get_width()
-        self.img_height = self.img.get_height()
-        self.img_X = (screen_width / 2) - (self.img_width / 2)
-        self.img_Y = 500
-        self.img_X_change = 0
-        self.img_Y_change = 0
+        self.scale = 1
+        unscaled_img = pygame.image.load('assets/ball_blue_small.png')
+        self.img = pygame.transform.rotozoom(
+            unscaled_img,
+            0,
+            self.scale
+        )
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+        self.x = (screen_width / 2) - (self.width / 2)
+        self.y = 500
+        self.x_change = 0
+        self.y_change = 0
+    def display(self):
+        screen.blit(self.img, (self.x, self.y))
+    def update(self):
+        self.img = pygame.transform.rotozoom(self.img, 0, self.scale)
 
-class Trajectory:
-    def __init__(self):
+class TrajectoryLine:
+    def __init__(self, ball):
         self.angle = 0
-        self.startpoint_x = ball_img_X + (ball_img_width / 2)
-        self.startpoint_y = ball_img_Y
-        endpoint_x = self.startpoint_x + 200 * math.sin(math.radians(self.angle))
-        endpoint_y = self.startpoint_y - 200 * math.cos(math.radians(self.angle))
-        self.endpoint = (endpoint_x, endpoint_y)
+        start_pos_x = ball.img.x + (ball.img.width / 2)
+        start_pos_y = ball.img.y
+        self.start_pos = (start_pos_x, start_pos_y)
+        end_pos_x = start_pos_x + 200 * math.sin(math.radians(self.angle))
+        end_pos_y = start_pos_y - 200 * math.cos(math.radians(self.angle))
+        self.end_pos = (end_pos_x, end_pos_y)
     def change_angle(self, angle):
         self.angle += angle
+    def display(self):
+        pygame.draw.line(screen, (255, 0, 0), self.start_pos, self.end_pos, 5)
 
-# Ball
-ball_img_original = pygame.image.load('assets/ball_blue_small.png')
-ball_scale = 1
-ball_img = pygame.transform.rotozoom(ball_img_original, 0, ball_scale)
-ball_img_width = ball_img.get_width()
-ball_img_height = ball_img.get_height()
-ball_img_X = (screen_width / 2) - (ball_img_width / 2)
-ball_img_Y = 500
-ball_img_X_change = 0
-ball_img_Y_change = 0
-
-trajectory_angle = 0
-# trajectory_shift_X =
-
-ballX = 0
-ballY = 0
-
-def ball(x,y):
-    screen.blit(ball_img, (x, y))
-
-def trajectory(x,y):
-    pygame.draw.line(screen, (255, 0, 0), (ball_img_X+(ball_img_width/2), ball_img_Y), (x, y), 5)
+ball = Ball()
+trajectory_line = TrajectoryLine(ball)
 
 running = True
-ball_moving = False
 frame_count = 0
 
 while running:
@@ -73,34 +69,22 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                ball_moving = True
+                ball.moving = True
             if event.key == pygame.K_LEFT:
-                trajectory_angle -= 10
+                trajectory_line.angle -= 10
             if event.key == pygame.K_RIGHT:
-                trajectory_angle += 10
-    if ball_moving and ball_img_Y >= 150:
-        ball_img_Y_change = -5
+                trajectory_line.angle += 10
+    if ball.moving and ball.img.y >= 150:
+        ball.img.y_change = -5
         frame_count += 1
         if frame_count % 2 == 0:  # Only rescale every 10 frames
-            ball_scale -= 0.02
-            ball_img = pygame.transform.rotozoom(ball_img_original, 0, ball_scale)
+            ball.img.scale -= 0.02
+            ball.img.update()
     else:
-        ball_img_Y_change = 0
-    ball_img_Y += ball_img_Y_change
-    ball(ball_img_X, ball_img_Y)
-    # trajectory(
-    #     ball_img_X+(ball_img_width/2),
-    #     400
-    # )
-    if not ball_moving:
-        trajectory_start_X = ball_img_X + (ball_img_width/2)
-        trajectory_start_Y = ball_img_Y
-        trajectory_length = 200  # Length of the trajectory line
-        trajectory_endpoint_X = trajectory_start_X + trajectory_length * math.sin(math.radians(trajectory_angle))
-        trajectory_endpoint_Y = trajectory_start_Y - trajectory_length * math.cos(math.radians(trajectory_angle))
-        trajectory(
-            trajectory_endpoint_X,
-            trajectory_endpoint_Y
-        )
+        ball.img.y_change = 0
+    ball.img.y += ball.img.y_change
+    ball.img.display()
+    if not ball.moving:
+        trajectory_line.display()
     pygame.display.update()
     clock.tick(60)
