30a1652329a9f33a85ae0a0e2ec197d0b1f1bb75
Bradley Austin <bradsthedeveloper@gmail.com>
2026-03-31 14:49:41 +0100

    Refactor ArduinoController: simplify states, add `connect` and `read` methods for streamlined serial communication

--- src/bksports/bowling/arduino.py
+++ src/bksports/bowling/arduino.py
@@ -1,4 +1,5 @@
 import time
+from collections.abc import Callable
 from enum import Enum
 
 import serial
@@ -6,29 +7,14 @@ import serial
 
 class ArduinoState(Enum):
     ERROR = -1
-    WAITING_FOR_CONNECTION = 0
-    PREPARING = 1
-    CALIBRATING = 2
-    CALIBRATED = 3
-    MEASURING = 4
-    FINISHED = 5
+    CONNECTING = 0
+    CONNECTED = 1
+    MEASURING = 2
+    FINISHED = 3
 
     @property
     def message(self) -> str:
         return f"{self.name.title()}."
-
-
-# class ArduinoStateError(Exception):
-#     """Exception raised for custom error in the application."""
-#
-#     MESSAGE
-#
-#     def __init__(self, value: int) -> None:
-#         self.value = value
-#         super().__init__(message)
-#
-#     def __str__(self) -> str:
-#         return f"{self.message}"
 
 
 def get_state(value: int) -> ArduinoState:
@@ -36,46 +22,78 @@ def get_state(value: int) -> ArduinoState:
         (state for state in list(ArduinoState) if state.value == value),
         ArduinoState.ERROR,
     )
-    # if output == ArduinoState.ERROR:
-    #     raise ArduinoStateError(
-    #         "Invalid state.",
-    #     )
     return output
+
+
+# def calculate_ball_speed(
+#     gyroscope_x_data: list[int], gyroscope_y_data: list[int],
+# ) -> float:
+#     gyroscope_x_max, gyroscope_y_max = max(g_x_data), max(g_y_data)
+#     return gyroscope_y_max
 
 
 class ArduinoController:
     def __init__(self) -> None:
+        self.state = ArduinoState.CONNECTING  # Waiting for Arduino input
         self.ser = serial.Serial("/dev/cu.usbmodem0000011", 9600, timeout=1)
-        self.state = (
-            ArduinoState.WAITING_FOR_CONNECTION
-        )  # Undefined, waiting for Arduino input
+        time.sleep(2)  # Wait for Arduino to start up
+        self.connect()
+
+    def connect(self) -> None:
+        """
+        Establishes connection to Arduino and prepares the program to read its serial port.
+
+        :raises KeyboardInterrupt: If the user interrupts the connection process.
+        """
+        try:
+            while self.state != ArduinoState.CONNECTED:
+                if self.ser.in_waiting > 0:  # Check if data is available
+                    line = self.ser.readline().decode("utf-8").rstrip()
+                    new_state_str, *str_data = line.split(",")
+                    self.state = get_state(int(new_state_str))
+        except KeyboardInterrupt:
+            print("\nProgram stopped by user.")
+            self.ser.close()
+            return
+        finally:
+            print(self.state.message)
+
+    def read(self, condition: Callable[[], bool]) -> tuple[list[int], list[int]]:
+        """
+        Reads serial data while a specific condition is met and returns the data measured in this period.
+
+        :param condition: A boolean-returning function, indicating whether the read operation should continue.
+        :return: A tuple containing the lists of data. (gyroscope_x_data, gyroscope_y_data)
+        """
+        if not self.ser.is_open:
+            print("Reopening serial port...")
+            self.ser.open()
+            time.sleep(2)
+        gyroscope_x_data = []
+        gyroscope_y_data = []
+        data_lists = (gyroscope_x_data, gyroscope_y_data)
+        try:
+            while self.ser.in_waiting <= 0:
+                time.sleep(0.1)
+            while condition():
+                if self.ser.in_waiting > 0:  # Check if data is available
+                    line = self.ser.readline().decode("utf-8").rstrip()
+                    new_state_str, *str_data = line.split(",")
+                    self.state = get_state(int(new_state_str))
+                    data = list(map(int, str_data))
+                    print(self.state.message, data)
+                    for i, p in enumerate(data):
+                        data_lists[i].append(p)
+        except KeyboardInterrupt:
+            print("\nProgram stopped by user.")
+        finally:
+            self.ser.close()
+        return data_lists
 
 
 if __name__ == "__main__":
-    # Configure the serial connection
-    # Replace 'COM3' with your actual port
     ac = ArduinoController()
-    time.sleep(2)  # Wait for connection to establish
-    print("Reading live data from Arduino...")
-    print()
-    try:
-        while True:
-            if ac.ser.in_waiting > 0:  # Check if data is available
-                line = ac.ser.readline().decode("utf-8").rstrip()
-                new_state_str, *str_data = line.split(",")
-                new_state = get_state(int(new_state_str))
-                data = list(map(int, str_data))
-                if new_state == ArduinoState.ERROR:
-                    print(f"Erroneous input: {line}")
-                # print(f"Received: {line}")
-                if ac.state != new_state:
-                    ac.state = new_state
-                    print(ac.state.message)
-                if ac.state == ArduinoState.MEASURING:
-                    print(data)
-                    gyroscope_x, gyroscope_y = data
-    except KeyboardInterrupt:
-        print("\nProgram stopped by user.")
-    finally:
-        ac.ser.close()  # Always close the port
-        print("Serial port closed.")
+    input("Press Enter to read")
+    end_time = time.time() + 10
+    g_x_data, g_y_data = ac.read(lambda: time.time() < end_time)
+    print(max(g_x_data), max(g_y_data))
