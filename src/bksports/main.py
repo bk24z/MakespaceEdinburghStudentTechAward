f8fb61eabe5e8e0421463b55da67317eec2ca243
Bradley Austin <bradsthedeveloper@gmail.com>
2026-05-24 19:07:50 +0100

    Add fullscreen secondary display logic

--- src/bksports/main.py
+++ src/bksports/main.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 from bksports.bowling.game import BowlingGame
@@ -5,7 +7,11 @@ from bksports.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 pygame.init()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# os.environ["SDL_VIDEO_MAC_FULLSCREEN_SPACES"] = "1"
+os.environ["SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS"] = "0"  # 0 = off
+os.environ["SDL_VIDEO_WINDOW_POS"] = "-1920,0"  # -1920 = secondary on left
+os.environ["SDL_VIDEO_FULLSCREEN_DISPLAY"] = "1"  # 1 = secondary
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 
 
@@ -16,6 +22,7 @@ def main() -> None:
         bowling_game.run()
         running = bowling_game.running
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
